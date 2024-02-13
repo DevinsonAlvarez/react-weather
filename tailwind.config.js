7522972c1a3610f32c734cdb99bd20f02f388a0c
Devinson Alvarez <devinson.20jemaine@gmail.com>
2024-02-13 14:59:13 -0500

    Setup project

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,21 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
-  ],
+  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        primary: '#3b82f6',
+        secondary: '#a855f7',
+        info: '#38bdf8',
+        success: '#22c55e',
+        warning: '#eab308',
+        danger: '#ef4444',
+        dark: '#0f172a',
+        white: '#f1f5f9',
+        black: '#020617',
+      },
+    },
   },
   plugins: [],
-}
-
+};
