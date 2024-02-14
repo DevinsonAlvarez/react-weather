d5569f8441e9a10ca235e190dfe1a64a4c174e63
Devinson Alvarez <devinson.20jemaine@gmail.com>
2024-02-14 11:47:01 -0500

    Run first format

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,19 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
-  darkMode: 'class',
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  darkMode: "class",
   theme: {
     extend: {
       colors: {
-        primary: '#3b82f6',
-        secondary: '#a855f7',
-        info: '#38bdf8',
-        success: '#22c55e',
-        warning: '#eab308',
-        danger: '#ef4444',
-        dark: '#0f172a',
-        white: '#f1f5f9',
-        black: '#020617',
+        primary: "#3b82f6",
+        secondary: "#a855f7",
+        info: "#38bdf8",
+        success: "#22c55e",
+        warning: "#eab308",
+        danger: "#ef4444",
+        dark: "#0f172a",
+        white: "#f1f5f9",
+        black: "#020617",
       },
     },
   },
